016c0c9ddd58dd781e18b5f9e56b8a3ccc9e3c5f
Arham Sarwar <148682737+Azan-Asim@users.noreply.github.com>
2025-08-18 17:55:03 +0500

    latest

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -22,7 +22,7 @@ export default function SignInPage() {
       <div className="mb-8">
         <Link href="/">
           <img
-                                                                                                                   src="/pasand-logo.svg"
+            src="/pasand-logo.svg"
             alt="Logo"
             className="h-16 w-auto mx-auto"
             style={{ maxHeight: 64 }}
